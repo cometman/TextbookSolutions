cdf2060141197030c0ad2fcb9b61e60df4820a94
Clayton Selby <csbuiss@gmail.com>
2016-03-03 14:08:36 -0600

    Added fetching instructor

--- main.rb
+++ main.rb
@@ -5,32 +5,56 @@ Bundler.require(:default)
 
 
 # Definitions
-@tstate_base = "http://txstate.verbacompare.com/compare/departments/"
+@tstate_base = "http://txstate.verbacompare.com/compare"
 @config = YAML.load_file('config.yml')
 
+# Retrieve departments for the given term.  Retry 3 times if there is a failure. Abort script after 3rd.
 def fetch_departments(term)
-  RestClient.get @tstate_base, {:params => { :term => term}}
-end
-
-def fetch_courses(department)
-  RestClient.get @tstate_base, {:params => { :id => department, :term => @config["term"]}}
-end
-
-def update_term(term)
-  departments_raw = fetch_departments(term)
   begin
     retries ||= 0
-    departments = JSON.parse(departments_raw)
+    JSON.parse(RestClient.get "#{@tstate_base}/departments", {:params => { :term => term}})
   rescue => e
-    puts "CRITICAL ERROR! #{e.message}....Retrying..."
+    puts "Fetch department critical error! #{e.message}....Retrying..."
+    sleep(5)
     retry if (retries += 1) < 3
     abort
   end
+end
+
+# Fetch courses for particular department.  Retry 3 times if there is a failure. Abort script after 3rd.
+def fetch_courses(department)
+  begin
+    retries ||= 0
+    JSON.parse(RestClient.get "#{@tstate_base}/courses", {:params => { :id => department, :term_id => @config["term"]}})
+  rescue => e
+    puts "Fetch course critical error! #{e.message}....Retrying..."
+    sleep(5)
+    retry if (retries += 1) < 3
+    abort
+  end  
+end
+
+# Fetch sections for particular course.  Retry 3 times if there is a failure. Abort script after 3rd.
+def fetch_sections(course)
+  begin
+    retries ||= 0
+    JSON.parse(RestClient.get "#{@tstate_base}/sections", {:params => { :id => course, :term_id => @config["term"]}})
+  rescue => e
+    puts "Fetch section critical error! #{e.message}....Retrying..."
+    sleep(5)
+    retry if (retries += 1) < 3
+    abort
+  end  
+end
+
+# Update config file with latest available departments. Return most recent departments
+def update_term_return_department(term)
+  departments = fetch_departments(term)
   if departments.count > 0
     puts "Departments found, trying next term"
     sleep(1)
     next_term = term + 1
-    update_term(next_term)
+    update_term_return_department(next_term)
   else
     @config["term"] = term - 1
     puts "No departments found. Most recent term being recorded as: #{@config["term"]}"
@@ -39,8 +63,28 @@ def update_term(term)
       h.write @config.to_yaml
     end
   end
+  return departments
 end
 
 # Execution
-update_term(@config["term"])
-#departments = fetch_departments(config["term"])
+all_departments = update_term_return_department(@config["term"])
+return_hash = {}
+# Add departments
+all_departments.each do |department|
+  return_hash[department["name"]] = {}
+  all_courses = fetch_courses(department["id"])
+
+  # Add courses to each department
+  all_courses.each do |course|
+    return_hash[department["name"]][course["name"]] = {}
+    all_sections = fetch_sections(course["id"])
+
+    # Add sections to each course
+    all_sections.each do |section|
+      return_hash[department["name"]][course["name"]][section["name"]] = {"instructor" => section["instructor"]}
+    end
+  end
+end
+byebug
+puts return_hash
+
